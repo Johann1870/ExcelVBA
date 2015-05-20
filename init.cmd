11fd666d101b5e8c2968ea67d5be882025b4f6d8
Johann1870 <Johann1870@users.noreply.github.com>
2015-05-19 17:12:24 -0700

    start from command regedit "Autorun"
    Place in path dir
    
    *This will set up your working environment*

---
+++ init.cmd
@@ -0,0 +1,32 @@
+@ECHO OFF
+
+	CD %WINDIR%\system32
+	
+	::ADMIN COLOR
+	SET ACOLOR=FC
+	
+	::USER COLOR
+	SET UCOLOR=8A
+	
+	::TITLE
+	SET TITLE1= - %USERDOMAIN%\%USERNAME% - %COMPUTERNAME%  LogonServer: %LOGONSERVER%
+	SET TITLE= %TITLE1%  Session:%SESSIONNAME%
+	
+	FSUTIL.exe > nul 2> nul && (COLOR %ACOLOR% & TITLE ADMIN %TITLE%) || (COLOR %UCOLOR% & TITLE NONADMIN %TITLE%)
+	
+	::Scripting path
+	SET path=%PATH%;J:\sc
+	
+	::Pandoc
+	SET path=%PATH%;J:\ex\Pandoc;%USERPROFILE%\AppData\Local\Pandoc
+	
+	::MiKTeX
+	SET path=%PATH%;J:\ex\MiKTeX\miktex-portable-2.9.5105\miktex\bin
+	
+	::PSTools
+	SET path=%PATH%;J:\PSTools
+	
+	::Git
+	SET path=%PATH%;%LOCALAPPDATA%\GitHub\PortableGit_c2ba306e536fdf878271f7fe636a147ff37326ad\cmd
+	
+@ECHO ON
